ef1bf122e36c675ee10f79a65338b02ab7143be3
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-23 19:50:10 +0100

    Added some level of authentication and authorization, also add

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -1,16 +1,20 @@
 class GearsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
     @gears = Gear.all
+    authorize @gears
   end
 
   def new
     @gear = Gear.new
+    authorize @gear
   end
 
   def create
     gear_owner = current_user
     @gear = Gear.new(gear_params)
     @gear.user = gear_owner
+    authorize @gear
     if @gear.save
       redirect_to root_path
     else
