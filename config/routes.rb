d3f0b23f23b98cefd87c6b6ad7bda48fa855cef9
Rafael Goncalves <rafaelnity@yahoo.com.br>
2022-08-23 18:23:26 -0300

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
   devise_for :users
+  
+  resources :booking, only: :index
+
   root to: "gears#home"
   resources :gears, only: %i[new create show edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
