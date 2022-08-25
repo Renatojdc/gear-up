a7de94a6a59f9bea39494576dc9c8abb2f05e3b7
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-25 22:22:58 +0100

    Add As an owner I can confirm a booking, Booking Cancelation for User, Authorization Pundit - Booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :gears
   has_many :bookings
+  has_many :gears_as_owners, through: :gears, source: :bookings
 
   validates :first_name, presence: true, length: { minimum: 3 }
   validates :last_name, presence: true, length: { minimum: 3 }
