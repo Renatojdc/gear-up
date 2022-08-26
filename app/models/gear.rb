d1e8528477671d48f4dcc0bd733f94ff7aa35684
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-26 13:08:17 +0100

    Change the booking system to the banner, and put default values to check-in and out

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -9,7 +9,6 @@ class Gear < ApplicationRecord
   validates :price, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 240 }
   validates :amount, presence: true
-  validates :photos, presence: true
 
   include PgSearch::Model
   pg_search_scope :search_location,
